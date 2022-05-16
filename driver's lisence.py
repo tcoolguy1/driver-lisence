0a1dcd52f987bceafdd35b1d006c490b1153d504
tcoolguy1 <78965454+tcoolguy1@users.noreply.github.com>
2022-05-16 19:02:58 +0530

    Add files via upload

---
+++ driver's lisence.py
@@ -0,0 +1,9 @@
+from tkinter import*
+root = Tk()
+root.title("Driving Lisence")
+root.geometry("500x500")
+label_id = Label(root)
+label_name = Label(root)
+label_dob = Label(root)
+label_pin = Label(root)
+
